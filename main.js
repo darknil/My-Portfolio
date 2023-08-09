0ea44bdfd2f4c6524ee9bec34046484398a19eed
darknil <jrmlelantus@gmail.com>
2023-08-09 09:54:32 +0200

    changed sidebar behavior

--- main.js
+++ main.js
@@ -2,14 +2,26 @@ const sideBarButton = document.getElementById('toggleButton');
 const sideBar = document.getElementById('sidebar');
 const navButtons = document.querySelectorAll('.nav_buttons');
 
+
+window.onload = () => {
+    if(window.innerWidth <= 912){
+        sideBar.classList.toggle('close');
+    }
+};
+
 sideBarButton.addEventListener('click', () => {
     sideBar.classList.toggle('close');
 });
 
 navButtons.forEach(button => {
     button.addEventListener('click', () => {
-        if (window.innerWidth <= 912) { // Проверяем ширину экрана
-            sideBar.classList.toggle('close');
-        }
+        sideBar.classList.toggle('close');
     });
+});
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 912) { 
+        sideBar.classList.add('close'); 
+    } else if(window.innerWidth > 912){
+        sideBar.classList.remove('close');
+    }
 });
