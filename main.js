81fb3da93e1102bb80ad6f479d397ff60d41faac
darknil <jrmlelantus@gmail.com>
2023-08-12 13:58:37 +0200

    fixed nav buttons

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
-const sideBarButton = document.getElementById('toggleButton');
-const sideBar = document.getElementById('sidebar');
-const navButtons = document.querySelectorAll('.nav_buttons');
-
+const sideBarButton = document.getElementById("toggleButton");
+const sideBar = document.getElementById("sidebar");
+const navButtons = document.querySelectorAll(".nav_buttons");
 
 window.onload = () => {
-    if(window.innerWidth <= 912){
-        sideBar.classList.toggle('close');
-    }
+  if (window.innerWidth <= 912) {
+    sideBar.classList.toggle("close");
+  }
 };
 
-sideBarButton.addEventListener('click', () => {
-    sideBar.classList.toggle('close');
+sideBarButton.addEventListener("click", () => {
+  sideBar.classList.toggle("close");
 });
 
-navButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        sideBar.classList.toggle('close');
-    });
-});
-window.addEventListener('resize', () => {
-    if (window.innerWidth <= 912) { 
-        sideBar.classList.add('close'); 
-    } else if(window.innerWidth > 912){
-        sideBar.classList.remove('close');
+navButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (window.innerWidth <= 912) {
+      sideBar.classList.toggle("close");
     }
+  });
+});
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 912) {
+    sideBar.classList.add("close");
+  } else if (window.innerWidth > 912) {
+    sideBar.classList.remove("close");
+  }
 });
